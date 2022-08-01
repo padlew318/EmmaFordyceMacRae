41e72fc0bad88fb88fd51b96d5beb6e992ecfbf4
Paddy Lewis <paddy.lewis@channelcentral.net>
2022-08-01 01:46:26 +0100

    removing duplicate files

--- V2/temp/header.php
+++
@@ -1,7 +0,0 @@
-    <?php include'var.php' ?>
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url; ?>css/screen.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url; ?>css/mc_yss_extensions.144201.css">
-    <script type="text/javascript" src="<?php echo $base_url; ?>js/utilities.js"></script>
-    <script type="text/javascript" src="<?php echo $base_url; ?>js/container_2.1.0.js"></script>
-    <script type="text/javascript" src="<?php echo $base_url; ?>js/yahoo.js"></script>
-	<link rel="shortcut icon" type="image/ico" href="favicon.ico">
