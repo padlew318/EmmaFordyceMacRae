358be8947f437383a95edc804f760ca5c295b6c0
Paddy Lewis <paddy.lewis@channelcentral.net>
2022-08-01 01:57:35 +0100

    clean up code

--- V3/landscapes/europe.php
+++ V3/landscapes/europe.php
@@ -5,38 +5,31 @@
     require ('../_includes/bs_header.php');
     require ('../_includes/bs_nav.php');
   ?>
+  
 <div class="container">
     
-    <div class="row">
-        
+    <div class="row">  
         <div class="pagetitle col-lg-12">
             <h1>Europe</h1>
             <h2>Emma Fordyce MacRae 20th Century American Artist</h2>
             <hr />
-              </div>
-        
-    </div>
-            <div class="row">
-        
-        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-8">
-                <a href="<?php echo $base_url; ?>_images/europe.jpg" alt="Europe" data-lightbox="Europe" data-caption="Europe"> 
-                <img src="<?php echo $base_url; ?>_images/europe.jpg" alt="Europe" data-lightbox="Europe" data-caption="Europe" class="img-responsive img-thumbnail"></a>
         </div>
-        
     </div>
             
-            <div class="row">
-         <div class="col-lg-12">  
-            <hr />
-													
-            <p>Europe<br>
-            Oil Painting<br>
-            Europe Landscape<br>
-            Emma Fordyce MacRae 1887 - 1974</p>
-				
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-8">
+            <a href="<?php echo $base_url; ?>_images/europe.jpg" alt="Europe" data-lightbox="Europe" data-caption="Europe"> 
+            <img src="<?php echo $base_url; ?>_images/europe.jpg" alt="Europe" data-lightbox="Europe" data-caption="Europe" class="img-responsive img-thumbnail"></a>
         </div>
-        
     </div>
+            
+    <div class="row">
+        <div class="col-lg-12">  
+            <hr />
+		    <p>Europe<br> Oil Painting<br> Europe Landscape<br> Emma Fordyce MacRae 1887 - 1974</p>
+		</div> 
+    </div>
+
     <hr />
    
 </div> <!-- /container -->
--- V3/landscapes/victory_girls.php
+++ V3/landscapes/victory_girls.php
@@ -5,35 +5,31 @@
     require ('../_includes/bs_header.php');
     require ('../_includes/bs_nav.php');
   ?>
+
 <div class="container">
     
-    <div class="row">
-        
+    <div class="row"> 
         <div class="pagetitle col-lg-12">
             <h1>Victory Girls</h1>
             <h2>Emma Fordyce MacRae 20th Century American Artist</h2>
             <hr />
-              </div>
-        
-    </div>
-            <div class="row">
-        
-        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-8">
-                <a href="<?php echo $base_url; ?>_images/victory_girls.jpg" alt="Victory Girls" data-lightbox="Victory Girls" data-caption="Victory Girls"> 
-                <img src="<?php echo $base_url; ?>_images/victory_girls.jpg" alt="Victory Girls" data-lightbox="Victory Girls" data-caption="Victory Girls" class="img-responsive img-thumbnail"></a>
         </div>
-        
     </div>
             
-            <div class="row">
-         <div class="col-lg-12">  
-            <hr />
-													
-            <p>Victory Girls<br>Oil Painting<br>New York Landscape<br>Emma Fordyce MacRae 1887 - 1974</p>
-				
-        </div>
-        
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-8">
+            <a href="<?php echo $base_url; ?>_images/victory_girls.jpg" alt="Victory Girls" data-lightbox="Victory Girls" data-caption="Victory Girls"> 
+            <img src="<?php echo $base_url; ?>_images/victory_girls.jpg" alt="Victory Girls" data-lightbox="Victory Girls" data-caption="Victory Girls" class="img-responsive img-thumbnail"></a>
+        </div> 
     </div>
+            
+    <div class="row">
+        <div class="col-lg-12">  
+            <hr />										
+            <p>Victory Girls<br>Oil Painting<br>New York Landscape<br>Emma Fordyce MacRae 1887 - 1974</p>	
+        </div>  
+    </div>
+    
     <hr />
    
 </div> <!-- /container -->
